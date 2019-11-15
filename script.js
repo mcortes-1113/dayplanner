6d0152d5eca1e8701c8d4cce7e07b7737360ff0d
Mario Cortes <mcortes.1113@gmail.com>
2019-11-14 21:55:56 -0600

    added code to populate today's date in header section

---
+++ script.js
@@ -0,0 +1,9 @@
+//$(document).ready(function(){
+
+//get current date, format and assign to variable
+var todayDate = moment().format("dddd, MMMM Do YYYY"); 
+
+//add date variable text to placeholder
+$("#plannerDate span").text(todayDate);
+
+//});
